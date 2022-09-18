32dc09be984639fccb28168b269ac3b2f6e83b36
Thomas Boutry <thomas.boutry@x3rus.com>
2022-09-18 16:49:01 -0400

    Feat(WebUI): Add URL management with RegEx ; Add album extraction for year

---
+++ src/albums-webui_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"testing"
+)
+
+// 		supportedPage: []string{"home", "listYear", "listMonth", "listDay"},
+
+// Validate Photo tags extraction Raw with a subset of files
+func TestUrlParse(t *testing.T) {
+	URL2Tests := []struct {
+		URL                 string
+		expectedWebPageType string
+		withError           bool
+	}{
+		{"/",
+			"home",
+			false,
+		},
+		{"/2020",
+			"listYear",
+			false,
+		},
+		{"/2020/02",
+			"listMonth",
+			false,
+		},
+		{"/2020/18",
+			"listMonth",
+			true,
+		},
+		{"/someTest",
+			"",
+			true,
+		},
+		{"/someTest/02/03",
+			"",
+			true,
+		},
+	}
+	// Loop in the list of photo
+	for _, tt := range URL2Tests {
+
+		flashbackWebUI := NewAlbumWebUI(nil, nil)
+		webPageType, err := flashbackWebUI.parseURL(tt.URL)
+
+		// ICI
+		if err != nil && !tt.withError {
+			t.Errorf("Error not expected, when parsing URL %s; got error : %v", tt.URL, err)
+		}
+
+		if webPageType != tt.expectedWebPageType {
+			t.Errorf("Web page type does not match what is expected for : %s : got %s want %s", tt.URL, webPageType, tt.expectedWebPageType)
+		}
+	}
+}
